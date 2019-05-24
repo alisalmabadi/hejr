9f9c391ca8826d66d50ef3b75002cf15faed27d7
reza.sh <reza.sh_rs@yahoo.com>
2019-05-24 19:42:33 +0430

    Update create.blade.php

--- resources/views/admin/eventSubject/create.blade.php
+++ resources/views/admin/eventSubject/create.blade.php
@@ -6,11 +6,11 @@
             <button type="submit" form="form-material" data-toggle="tooltip" title="" class="btn btn-primary" data-original-title="ذخیره"><i class="fa fa-save"></i></button>
             <a href="{{route('admin.eventSubject.index')}}" data-toggle="tooltip" title="" class="btn btn-default" data-original-title="لغو"><i class="fa fa-reply"></i></a></div>
         <h1>
-            add new eventSubject
+           موضوع رویداد
         </h1>
         <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-bars"></i>event subjects</a></li>
-            <li class="active">add new event subject</li>
+            <li><a href="#"><i class="fa fa-bars"></i>موضوع رویداد</a></li>
+            <li class="active">اضافه کردن موضوع رویداد</li>
         </ol>
     </section>
 @endsection
@@ -21,7 +21,7 @@
 
         <div class="panel panel-default">
             <div class="panel-heading">
-                <h3 class="panel-title"><i class="fa fa-plus "></i>ایجاد هسته جدید</h3>
+                <h3 class="panel-title"><i class="fa fa-plus "></i>اضافه کردن موضوع رویداد</h3>
             </div>
             <div class="panel-body">
                 <form action="{{route('admin.eventSubject.store')}}" class="form-horizontal" method="post" enctype="multipart/form-data" id="form-material">
