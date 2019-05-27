3a12c556ec2edd052c5a2e1c6b525cfe3edd2706
seyyed ali salmabadi <s.alisalmabadi@yahoo.com>
2019-05-27 23:14:02 +0430

    Update index.blade.php

--- resources/views/user/event/index.blade.php
+++ resources/views/user/event/index.blade.php
@@ -31,14 +31,15 @@
                     <div class="m-portlet__head m-portlet__head--fit">
                         <div class="m-portlet__head-caption">
                             <div class="m-portlet__head-action">
-                                <button type="button" class="btn btn-sm m-btn--pill  btn-brand">Blog</button>
+                                <button type="button" class="btn m-btn m-btn--gradient-from-danger m-btn--gradient-to-warning">{{$event->event_status->name}}</button>
+
                             </div>
                         </div>
                     </div>
                     <div class="m-portlet__body">
                         <div class="m-widget19">
                             <div class="m-widget19__pic m-portlet-fit--top m-portlet-fit--sides" style="min-height-: 286px">
-                                <img src="../../assets/app/media/img//blog/blog1.jpg" alt="">
+                                <img src="{{asset('img//blog/blog1.jpg')}}" alt="">
                                 <h3 class="m-widget19__title m--font-light">
                                     {{$event->name}}
                                 </h3>
@@ -47,37 +48,42 @@
                             <div class="m-widget19__content">
                                 <div class="m-widget19__header">
                                     <div class="m-widget19__user-img">
-                                        <img class="m-widget19__img" src="../../assets/app/media/img//users/user1.jpg" alt="">
+                                        <img class="m-widget19__img" src="{{asset('img/icons/warning.svg')}}" alt="">
                                     </div>
                                     <div class="m-widget19__info">
 														<span class="m-widget19__username">
 															{{$event->center_core->name}}
 														</span><br>
                                         <span class="m-widget19__time">
-															قیمت {{$event->price }}
+															قیمت {{number_format($event->price)}}
+                                            تومان
 														</span>
                                     </div>
                                     <div class="m-widget19__stats">
-														<span class="m-widget19__number m--font-brand">
-															{{$event->capacity}}
-														</span>
+														<span class="m-widget19__number m--font-brand">{{$event->capacity}}</span>
                                         <span class="m-widget19__comment">
 															ظرفیت
-														</span>
+				</span>
                                     </div>
                                 </div>
                                 {{--body1 place--}}
-                                <div class="m-widget19__body body1_{{$event->id}}" style="display:block;">
-                                    <p>{{$event->description}}</p><hr/>
+                                <div class="m-widget19__body body1_{{$event->id}} text-center" style="display:block;">
+                                    <p>{!! $event->description !!}</p><hr/>
                                     <label>تاریخ دوره</label><br>
-                                    <p class="pull-left">از {{$event->start_date}} تا {{$event->end_date}}</p>
+                                    <p class="btn btn-secondary m-btn m-btn--icon m-btn--pill">از {{$event->start_dates}} تا {{$event->end_dates}}</p>
                                     <label>تاریخ پایان ثبت نام</label><br>
-                                    <p class="pull-left">{{$event->end_date_signup}}</p>
+                                    <p class="btn btn-secondary m-btn m-btn--icon m-btn--pill">{{$event->end_date_sign}}</p>
                                     <br/>
                                     <hr/>
-                                    <p>موضوع دوره: {{$event->event_subject->name}}</p>
-                                    <p>نوع دوره: {{$event->event_type->name}}</p>
-                                    <p>وضعیت دوره: {{$event->event_status->name}}</p>
+                                    <p>موضوع دوره:
+  <button class="btn m-btn m-btn--gradient-from-info m-btn--gradient-to-accent" type="btn">                                      {{$event->event_subject->name}}
+  </button>
+                                    </p>
+                                    <p class="text-center">نوع دوره:
+     <button type="button" class="btn m-btn m-btn--gradient-from-primary m-btn--gradient-to-info">                                   {{$event->event_type->name}}
+     </button>
+                                    </p>
+                                  {{--  <p>وضعیت دوره: {{$event->event_status->name}}</p>--}}
                                     <hr/>
                                     <p>آدرس: {{$event->provinces->name}} - {{$event->cities->name}} ...</p>
                                 </div>
@@ -89,7 +95,6 @@
                                     <p>{{$event->long_description}}</p><hr/>
                                     <label>آدرس</label>
                                     <p>{{$event->provinces->name}}, {{$event->cities->name}}, {{$event->address}}</p>
-                                    <div class="text-center" style="height: 100px;border: 2px solid #e5e5e5;">address points here</div>
                                 </div>
                                 {{--end of body2 place--}}
 
