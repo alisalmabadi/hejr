0756f07b037ecd772a72f308f99c7ebd25d9b70b
seyyed ali salmabadi <s.alisalmabadi@yahoo.com>
2019-07-11 00:00:42 +0430

    Update show_registered.blade.php

--- resources/views/user/event/show_registered.blade.php
+++ resources/views/user/event/show_registered.blade.php
@@ -13,9 +13,29 @@
     <!-- BEGIN: Subheader -->
     <div class="m-subheader ">
         <div class="d-flex align-items-center">
-            <div class="mr-auto">
-                <h3 class="m-subheader__title ">رویداد {{$eventUser->event->name}}</h3>
+            <div class="margin-right-auto">
+                <h3 class="m-subheader__title m-subheader__title--separator">رویداد ثبت نام شده</h3>
+                <ul class="m-subheader__breadcrumbs m-nav m-nav--inline">
+                    <li class="m-nav__item m-nav__item--home">
+                        <a href="#" class="m-nav__link m-nav__link--icon">
+                            <i class="m-nav__link-icon la la-home"></i>
+                        </a>
+                    </li>
+                    <li class="m-nav__separator">-</li>
+                    <li class="m-nav__item">
+                        <a href="" class="m-nav__link">
+                            <span class="m-nav__link-text">رویداد ها</span>
+                        </a>
+                    </li>
+                    <li class="m-nav__separator">-</li>
+                    <li class="m-nav__item">
+                        <a href="" class="m-nav__link">
+                            <span class="m-nav__link-text">رویداد ثبت نام شده</span>
+                        </a>
+                    </li>
+                </ul>
             </div>
+
         </div>
     </div>
 
@@ -32,8 +52,8 @@
                     </thead>
                     <tbody>
                         <tr>
-                            <td>{{$eventUser->user->name .' '. $eventUser->user->lastname}}</td>
-                            <td>{{$eventUser->user->nationcode}}</td>
+                            <td> {{$eventUser->user->name .' '. $eventUser->user->lastname}}</td>
+                            <td>{{$eventUser->user->nationcode or 'ثبت نشده'}}</td>
                             <td>{{$eventUser->user->phonenumber}}</td>
                         </tr>
                     </tbody>
@@ -51,8 +71,8 @@
                     </thead>
                     <tbody>
                         <tr>
-                            <td>{{$eventUser->event->name}}</td>
-                            <td>{{$eventUser->event->description}}</td>
+                           <td>{{$eventUser->event->name}}</td>
+                            <td>{!! $eventUser->event->long_description !!}</td>
                             <td>{{$eventUser->event->price}}</td>
                         </tr>
                     </tbody>
