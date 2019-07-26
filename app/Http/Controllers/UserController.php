7c9a8f5b9a07e449d2b226d6193d8ca90bece01c
reza.sh <reza.sh_rs@yahoo.com>
2019-07-26 23:54:59 +0430

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -32,7 +32,7 @@ class UserController extends Controller
     public function __construct()
     {
          $this->middleware('admin')->only(['index','create','edit','store','update','index','multiple','multiple_store','show_users']);
-         $this->middleware('auth')->only(['panel','profile','createEvent']);
+         $this->middleware('auth')->only(['panel','profile','createEvent', 'core_users_index', 'core_users_create', 'core_users_store', 'core_users_edit', 'core_users_update']);
     }
 
     /**
@@ -725,5 +725,190 @@ $this->validate($request,[
         $uni = new University();
         $uni = $uni->create($request->all());
         return response($uni);
+    }
+
+    public function core_users_index()
+    {
+        $admin_user = \Auth::guard('web')->user();
+        $users = $admin_user->core->users;
+        return view('user.coreUsers.index', compact('users'));
+    }
+
+    public function core_users_show(User $user)
+    {
+        return response($user);
+    }
+
+    public function core_users_create()
+    {
+        $core = \Auth::guard('web')->user()->core_id;
+        $areas=Area::all();
+        $provinces=Province::all();
+        $soldier_services=SoldierServices::all();
+        $grades=Grade::all();
+        $fields=Field::where('status',1);
+        $universities=University::all();
+        $jobs=Job::all();
+        return view('user.coreUsers.create',compact('core','areas','soldier_services','grades','fields','universities','jobs','cities','provinces'));
+    }
+
+    public function core_users_store(Request $request)
+    {
+        $this->validate($request,[
+            'name'=>'required',
+            'lastname'=>'required',
+            'username'=>'required|unique:users',
+            'password'=>'required',
+            'email'=>'email|unique:users',
+        ],[
+            'name.required'=>'نام اجباری است.',
+            'lastname.required'=>'نام خانوادگی اجباری است.',
+            'password.required'=>'رمزعبور اجباری است.',
+            'username.required'=>'نام کاربری اجباری است.',
+            'username.unique'=>'نام کاربری تکراری است.',
+            'email.required'=>'ایمیل را وارد کنید',
+            'email.unique'=>'این ایمیل قبلا استفاده شده است !'
+        ]);
+        if(isset($request->birthday)) {
+            /*$date = explode('/', $request->birthday);
+            $date = Verta::getGregorian($date[0], $date[1], $date[2]);
+            $dated = $date[0] . '-' . $date[1] . '-' . $date[2];*/
+            $dated=Convertnumber2english($request->birthday);
+        }else{
+            $dated=null;
+        }
+    
+        $user=User::create([
+            'name'=>$request->name,
+            'lastname'=>$request->lastname,
+            'username'=>$request->username,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password),
+            'bio'=>$request->bio,
+/*         'city_id'=>$request->city_id,
+            'province_id'=>$request->province_id,*/
+            'grade_id'=>$request->grade_id,
+            'soldier_service_id'=>$request->soldier_service_id,
+            'field_id'=>$request->field_id,
+            'university_id'=>$request->university_id,
+            'core_id'=>$request->core_id,
+            'area_id'=>$request->area_id,
+            'job_id'=>$request->job_id,
+            'nationcode'=>$request->nationcode,
+            'father_name'=>$request->father_name,
+            'phonenumber'=>$request->phonenumber,
+            'image_path'=>$request->image_path,
+            'birthday'=>$dated,
+            'martial'=>$request->martial,
+            'status'=>$request->status,
+            'address'=>$request->address,
+            'konkor_grade'=>$request->konkor_grade
+
+        ]);
+            flashs('عضو با موفقیت افزوده شد.','success');
+            return redirect()->route('user.coreUsers.index');
+    }
+
+    public function core_users_edit(User $user)
+    {
+        $core = \Auth::guard('web')->user()->core_id;
+        $areas=Area::all();
+        $provinces=Province::all();
+        $soldier_services=SoldierServices::all();
+        $grades=Grade::all();
+        $fields=Field::where('status',1);
+        $universities=University::all();
+        $jobs=Job::all();
+        return view('user.coreUsers.edit',compact('user', 'core','areas','soldier_services','grades','fields','universities','jobs','cities','provinces'));
+    }
+
+    public function core_users_update(Request $request, User $user)
+    {
+        $this->validate($request,[
+            'name'=>'required',
+            'lastname'=>'required',
+            'username'=>'required|unique:users,username,'.$user->id,
+/*            'password'=>'required',*/
+            'email'=>'required|unique:users,email,'.$user->id,
+
+
+        ],[
+            'name.required'=>'نام اجباری است.',
+            'lastname.required'=>'نام خانوادگی اجباری است.',
+/*            'password.required'=>'رمزعبور اجباری است.',*/
+            'username.required'=>'نام کاربری اجباری است.',
+            'username.unique'=>'نام کاربری تکراری است.',
+            'email.required'=>'ایمیل را وارد کنید.'
+
+        ]);
+        if(isset($request->birthday)) {
+           /* $date = explode('/', $request->birthday);
+            $date = Verta::getGregorian($date[0], $date[1], $date[2]);
+            $dated = $date[0] . '-' . $date[1] . '-' . $date[2];*/
+           $dated=Convertnumber2english($request->birthday);
+        }else{
+            $dated=null;
+        }
+        if(isset($request->password)) {
+
+            /*dd($request->all());*/
+            $user->update([
+                'name' => $request->name,
+                'lastname' => $request->lastname,
+                'username' => $request->username,
+                'email' => $request->email,
+                'password' => bcrypt($request->password),
+                'bio' => $request->bio,
+/*                'city_id' => $request->city_id,*/
+/*                'province_id' => $request->province_id,*/
+                'grade_id' => $request->grade_id,
+                'soldier_service_id' => $request->soldier_service_id,
+                'field_id' => $request->field_id,
+                'university_id' => $request->university_id,
+                'core_id' => $request->core_id,
+                'area_id'=>$request->area_id,
+                'job_id'=>$request->job_id,
+                'nationcode' => $request->nationcode,
+                'father_name' => $request->father_name,
+                'phonenumber' => $request->phonenumber,
+                'image_path' => $request->image_path,
+                'birthday' => $dated,
+                'martial'=>$request->martial,
+                'status'=>$request->status,
+                'address'=>$request->address,
+                 'konkor_grade'=>$request->konkor_grade
+
+            ]);
+        }else{
+            $user->update([
+                'name' => $request->name,
+                'lastname' => $request->lastname,
+                'username' => $request->username,
+                'email' => $request->email,
+/*                'password' => bcrypt($request->password),*/
+                'bio' => $request->bio,
+/*                'city_id' => $request->city_id,*/
+/*                'province_id' => $request->province_id,*/
+                'grade_id' => $request->grade_id,
+                'soldier_service_id' => $request->soldier_service_id,
+                'field_id' => $request->field_id,
+                'university_id' => $request->university_id,
+                'core_id' => $request->core_id,
+                'area_id'=>$request->area_id,
+                'job_id'=>$request->job_id,
+                'nationcode' => $request->nationcode,
+                'father_name' => $request->father_name,
+                'phonenumber' => $request->phonenumber,
+                'image_path' => $request->image_path,
+                'birthday' => $dated,
+                'martial'=>$request->martial,
+                'status'=>$request->status,
+                'address'=>$request->address,
+                'konkor_grade'=>$request->konkor_grade
+
+            ]);
+        }
+        flashs('عضو با موفقیت ویرایش شد.','success');
+        return redirect()->route('user.coreUsers.index');
     }
 }
