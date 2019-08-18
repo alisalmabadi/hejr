d805ddc9cbb2d4ae57f650485f4263ed4fb50bad
seyyed ali salmabadi <s.alisalmabadi@yahoo.com>
2019-08-18 17:19:20 +0430

    Update layout.blade.php

--- resources/views/errors/layout.blade.php
+++ resources/views/errors/layout.blade.php
@@ -1,57 +1,110 @@
+
+
+
+
+
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>@yield('title')</title>
+<html lang="en-us" class="no-js">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+<head>
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <meta name="description" content="The description should optimally be between 150-160 characters.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- ============== Resources style ============== -->
+    <link rel="stylesheet" type="text/css" href="{{asset('css/admin-app.css')}}" />
+    <link rel="stylesheet" type="text/css" href="{{asset('css/error.css')}}" />
+</head>
 
-            .full-height {
-                height: 100vh;
-            }
+<body class="flat">
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+<!-- Canvas for particles animation -->
+<div id="particles-js"></div>
 
-            .position-ref {
-                position: relative;
-            }
+<!-- Your logo on the top left -->
+<a href="{{URL::previous()}}" class="logo-link" title="بازگشت">
 
-            .content {
-                text-align: center;
-            }
+    <img src="{{asset('images/logo.jpg')}}" class="logo" alt="بازگشت" />
 
-            .title {
-                font-size: 36px;
-                padding: 20px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title">
-                    @yield('message')
-                </div>
+</a>
+
+<div class="content">
+
+    <div class="content-box">
+
+        <div class="big-content">
+
+            <!-- Main squares for the content logo in the background -->
+            <div class="list-square">
+                <span class="square"></span>
+                <span class="square"></span>
+                <span class="square"></span>
             </div>
+
+            <!-- Main lines for the content logo in the background -->
+            <div class="list-line">
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+            </div>
+
+            <!-- The animated searching tool -->
+            <i class="fa fa-search" aria-hidden="true"></i>
+
+            <!-- div clearing the float -->
+            <div class="clear"></div>
+
         </div>
-    </body>
+
+        <!-- Your text -->
+        <h1> @yield('message')</h1>
+        @yield('des')
+
+    </div>
+
+</div>
+
+{{--
+<footer class="light">
+
+    <ul>
+        <li><a href="#">خانه</a></li>
+
+        <li><a href="#">جستجو</a></li>
+
+        <li><a href="#">راهنما</a></li>
+
+        <li><a href="#">قوانین و مقررات</a></li>
+
+        <li><a href="#">نقشه سایت</a></li>
+
+        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+
+        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+    </ul>
+
+</footer>
+--}}
+
+<!-- ///////////////////\\\\\\\\\\\\\\\\\\\ -->
+<!-- ********** jQuery Resources ********** -->
+<!-- \\\\\\\\\\\\\\\\\\\/////////////////// -->
+
+<!-- * Libraries jQuery and Bootstrap - Be careful to not remove them * -->
+
+<script src="{{asset('admin-app.js')}}"></script>
+{{--
+<script src="{{asset('js/jquery.js')}}"></script>
+<script src="js/bootstrap.min.js"></script>--}}
+
+<!-- Particles plugin -->
+<script src="{{asset('js/particles.js')}}"></script>
+
+</body>
+
 </html>
