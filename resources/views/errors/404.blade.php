c0067206775a976974693beac61b38d41b875e29
seyyed ali salmabadi <s.alisalmabadi@yahoo.com>
2019-08-18 17:19:13 +0430

    changed 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,11 @@
 @extends('errors::layout')
 
-@section('title', 'Page Not Found')
+@section('title', 'صفحه مورد نظر یافت نشد')
 
 @section('message', 'متاسفانه صفحه مورد نظر یافت نشد!')
+@section('des')
+
+    <p>این صفحه وجود ندارد و یا آدرس اشتباهی است<br>
+        احتمالاً صفحه مورد نظر انتقال یافته است</p>
+
+ @endsection
