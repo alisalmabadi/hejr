686a62fa22360f82d10a3d8b65164e13558f459b
reza.sh <reza.sh_rs@yahoo.com>
2019-08-24 22:26:13 +0430

    Update edit.blade.php

--- resources/views/admin/event/edit.blade.php
+++ resources/views/admin/event/edit.blade.php
@@ -306,8 +306,14 @@
                                             <div class="col-md-9">
                                                 <img class="img-responsive" src="{{asset($image->image_path)}}" style="width:400px; margin-right:220px;">
                                             </div>
-                                            <div class="col-md-3">
+                                            <div class="col-md-3 not_thumbnail_{{$image->id}}" style="@if($event->thumbnail_id == $image->id) display:none; @endif">
                                                 <button type="button" class="btn btn-danger form-control btn-delete_img" data-img_id="{{$image->id}}" data-event_id="{{$event->id}}">حذف این تصویر</button>
+                                                <hr/>
+                                                <button type="button" class="btn btn-warning form-control btn-change_thumbnail" data-img_id="{{$image->id}}" data-event_id="{{$event->id}}">انتخاب به عنوان عکس شاخص</button>
+                                            </div>
+
+                                            <div class="col-md-3 is_thumbnail_{{$image->id}} label-success" style="border-radius: 30px;text-align: center; @if($event->thumbnail_id != $image->id) display:none; @endif">
+                                                <label class="">تصویر شاخص <br/> برای حذف این تصویر ابتدا باید یکی دیگر از تصاویر موجود را به عنوان تصویر شاخص انتخاب کنید!</label>
                                             </div>
                                         </div>
                                         @endforeach
@@ -462,4 +468,56 @@
         });
     </script>
     {{--end of delete image--}}
+
+    {{-- change thumbnail image --}}
+    <script>
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN' : $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+        $(".btn-change_thumbnail").on("click", function(){
+            swal.fire({
+                title: 'تغییر تصویر شاخص',
+                text: "اطمینان دارید ؟",
+                type: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'بله, تصویر شاخص تغییر کند.',
+                cancelButtonText: 'خیر'
+                }).then((result) => {
+                if (result.value) {
+
+                    var event_id = $(this).data('event_id');
+                    var image_id = $(this).data('img_id');
+                    var url = "{{route('admin.event.change_thumbnail_image')}}";
+                    $.ajax({
+                        data:{'event_id':event_id, 'image_id':image_id},
+                        url:url,
+                        type:"POST",
+                        success:function(data){
+                            swal.fire({
+                                position: 'top-left',
+                                type: 'success',
+                                title: 'تصویر شاخص با موفقیت تغییر کرد',
+                                showConfirmButton: false,
+                                timer: 1500
+                            });
+                            //field haye ghabli ok she
+                            $(".is_thumbnail_"+data).fadeOut("slow");
+                            $(".not_thumbnail_"+data).fadeIn("slow");
+                            //field haye alana ok she
+                            $(".not_thumbnail_"+image_id).fadeOut("slow");
+                            $(".is_thumbnail_"+image_id).fadeIn("slow");
+                        },
+                        error:function(){
+                            console.log('errror in changeing thumbnail image');
+                        }
+                    });
+                }
+            })
+        });
+    </script>
+    {{-- end of chnge thumbnail image --}}
 @stop
